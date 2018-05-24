c34c05a6ff89d01be25355deaec534cb5fdc800e
lucasPasquotto <lucaspasquotto@outlook.com>
2018-05-23 21:34:18 -0300

    Fist projetc, have fun.

---
+++ app/src/main/java/adivinha/cursoandroid/com/adivinha/MainActivity.java
@@ -0,0 +1,35 @@
+package adivinha.cursoandroid.com.adivinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = (Button) findViewById(R.id.botaoJogarId);
+        textoResultado = (TextView) findViewById(R.id.resultadoId);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random randomico = new Random();
+                textoResultado.setText("Numero escolhido: " + randomico.nextInt(10));
+            }
+        });
+
+    }
+}
